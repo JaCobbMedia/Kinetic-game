90de9ab42954a2bf2a01601a9105fea60205c10a
JaCobbMedia <joksavodnikas@gmail.com>
2018-11-04 23:52:15 +0200

    Created laser gun

---
+++ Kinetic-game/Assets/Scripts/LaserSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserSpawner : MonoBehaviour {
+
+    private LineRenderer lineRenderer;
+    public Transform laserHitPoint;
+
+
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        lineRenderer.useWorldSpace = true;
+	}
+	
+	void Update () {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up);
+
+        laserHitPoint.position = hit.point;
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, laserHitPoint.position);
+
+        if(lineRenderer.enabled == true && hit.collider.tag == "Player")
+        {
+            Laser_mechanics laser_Mechanics = GameObject.Find("Laser_gun").GetComponent<Laser_mechanics>();
+            laser_Mechanics.HitTarget(hit.collider.gameObject);
+        }
+
+	}
+
+    public IEnumerator ShootLaser()
+    {
+        yield return new WaitForSeconds(3);
+        lineRenderer.enabled = true;
+    }
+
+    public IEnumerator DisableLaser()
+    {
+        yield return new WaitForSeconds(2);
+        lineRenderer.enabled = false;
+    }
+}
---
+++ Kinetic-game/Assets/Scripts/Laser_mechanics.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser_mechanics : MonoBehaviour {
+
+    private Quaternion rotation = new Quaternion(0,0,180,1);
+    private Quaternion desiredRot;
+    private GameObject target;
+    private LaserSpawner laserSpawner;
+    public float rotationSpeed = 10;
+
+    private void Start()
+    {
+        target = GameObject.FindWithTag("Player");
+        laserSpawner = GameObject.Find("Laser_beam_spawn").GetComponent<LaserSpawner>();
+    }
+
+    void Update () {
+
+        if (target != null)
+        {
+            var targetDistance = Vector3.Distance(transform.position, target.transform.position);
+
+            if (targetDistance < 13.0f)
+            {
+                    rotation = Quaternion.LookRotation(target.transform.position - transform.position, transform.TransformDirection(Vector3.down));
+                    desiredRot = new Quaternion(0, 0, rotation.z, rotation.w);
+                    StartCoroutine(laserSpawner.ShootLaser());
+            }
+            else
+            {
+                desiredRot = new Quaternion(0, 0, 0, 1);
+                StartCoroutine(laserSpawner.DisableLaser());
+            }
+        }
+        else
+        {
+            desiredRot = new Quaternion(0, 0, 0, 1);
+            StartCoroutine(laserSpawner.DisableLaser());
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRot, rotationSpeed * Time.deltaTime);
+    }
+
+    public void HitTarget(GameObject hitTarget)
+    {
+        Destroy(hitTarget);
+        target = null;
+    }
+}
