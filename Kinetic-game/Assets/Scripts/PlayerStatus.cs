33c9a046029c5b566b8334f582b68e478dea534e
JaCobbMedia <joksavodnikas@gmail.com>
2018-12-07 03:28:25 +0200

    player fixes

--- Kinetic-game/Assets/Scripts/PlayerStatus.cs
+++ Kinetic-game/Assets/Scripts/PlayerStatus.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour {
 
-
     public float health;
+    private Animator animator;
+    public Transform spawn;
+    private PlayerMovement playerMovement;
 
 	void Start () {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
 	}
 	
 	void Update () {
 
-        if(health <= 0)
+        animator.SetFloat("health", health);
+        if (health <= 0)
         {
-            Destroy(gameObject);
+            StartCoroutine(Respawn());
         }
 		
 	}
@@ -27,5 +32,16 @@ public class PlayerStatus : MonoBehaviour {
     public void RegenerateHealth(float add)
     {
         health += add;
+    }
+
+    private IEnumerator Respawn()
+    {
+        Debug.Log("playerStatus");
+        playerMovement.ChangeControlStatus(false);
+        yield return new WaitForSeconds(2);
+        health = 100;
+        gameObject.transform.position = spawn.position;
+        yield return new WaitForSeconds(2);
+        playerMovement.ChangeControlStatus(true);
     }
 }
