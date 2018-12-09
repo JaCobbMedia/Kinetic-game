4f3a181cdf7a370f6ac27d1e450f0ff676ceb4fb
JaCobbMedia <joksavodnikas@gmail.com>
2018-12-09 20:27:44 +0200

    Connected all levels

---
+++ Kinetic-game/Assets/Scripts/LevelFinish.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFinish : MonoBehaviour {
+
+    public int sceneNumber;
+    public ParticleSystem particles;
+
+	void Start () {
+        particles = GetComponent<ParticleSystem>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            particles.Play();
+            StartCoroutine(WaitForLevelEnd());
+        }
+    }
+
+    private IEnumerator WaitForLevelEnd()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+}
